784dbad0c6c058cd53933d322c845ca894813202
SaulToscano <saul.toscano.robles@gmail.com>
2023-08-05 12:27:23 -0700

    img test

--- components/About.vue
+++ components/About.vue
@@ -1,7 +1,3 @@
-<script setup>
-  import ImgReact from '../public/react.png'
-</script>
-
 <template>
   <section id="about">
     <div class="tile tile-1">
@@ -11,7 +7,7 @@
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
           tempore accusantium voluptates corrupti facere culpa necessitatibus,
         </p>
-        <img :src="ImgReact" alt="" width="200" />
+        <img src="https://i.ibb.co/CMZDzKx/react.png" alt="" width="200" />
       </div>
     </div>
     <div class="tile tile-2">
